3dd3aaac9e1181e66e038fe6adcadeceb105bd1d
Chinnapatz <mr.chronos038@gmail.com>
2023-09-30 21:11:06 +0700

    add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Payment from './pages/payment_coin/payment_coin';
+
+function App() {
+  return (
+    <>
+      <Payment/>
+    </>
+  );
+}
+
+export default App;
