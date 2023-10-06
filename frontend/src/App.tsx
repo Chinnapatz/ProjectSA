a53b5f6288c8eb5773a3f02ebe99c4859edd9dc3
Poomtawan Pimprom <poomtawan.work@gmail.com>
2023-10-06 22:43:45 +0700

    update Page.dashboard(80%)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,10 @@ import {
   Routes,
 } from "react-router-dom";
 //page
-import Payment from './pages/payment_coin/payment_coin';
 import Login  from './pages/login/login';
 import Register from "./pages/register/register";
+import Home from "./pages/dashboard/dashboard";
+import Payment from './pages/payment_coin/payment_coin';
 import Publish_Se from "./pages/publish_episodes/publish_se";
 import Publish_Ep from "./pages/publish_series/publish_ep";
 import Bookshelf_followed from "./pages/bookshelf/bookshelf_followed";
@@ -22,7 +23,7 @@ function App() {
         <Route path='/' element={<Login />}/>
         <Route path='/Buycoin' element={<Payment />}/>
         <Route path='/Register' element={<Register/>}/>
-        {/* <Route path='/Home' element={<Dashboard/>}/> */}
+        <Route path='/Home' element={<Home/>}/>
         {/* <Route path='/Bookshelf' element={<Bookshelf/>}/>
         <Route path='/Publish' element={<Publish/>}/> */}
         <Route path='/Publish_Se' element={<Publish_Se/>}/> 
