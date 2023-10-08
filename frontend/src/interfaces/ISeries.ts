4f6aa7034b222b4e333649c3ed26981633b25077
Chinnapatz <mr.chronos038@gmail.com>
2023-10-08 15:55:39 +0700

    add controller series

---
+++ frontend/src/interfaces/ISeries.ts
@@ -0,0 +1,9 @@
+export interface SeriesInterface {
+    CartoonID ?:            number;  
+    Title ?:	            string; 
+	Catagories ?:			string; 
+	Summary	?:			    string; 
+	Square_Thumbnail ?:	    Blob; 
+	Vertical_Thumbnail ?:	Blob; 
+	Datetime ?:			    Date; 
+}
